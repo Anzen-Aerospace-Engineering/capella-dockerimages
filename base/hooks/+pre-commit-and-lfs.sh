9cd9fcf5b40a2d2d91c2efdef615b6278c365fbb
manuel-ml-atica <manuelmartin@anzenengineering.com>
2024-08-07 14:07:36 +0200

    [test]

--- base/hooks/+pre-commit-and-lfs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# SPDX-FileCopyrightText: Copyright DB InfraGO AG and contributors
-# SPDX-License-Identifier: Apache-2.0
-
-STAGE=$(basename "$0")
-
-/opt/git/global-hooks/+pre-commit.sh $STAGE "$@" < /dev/null
-exec git lfs $STAGE "$@"
--- base/hooks/+pre-commit-only.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# SPDX-FileCopyrightText: Copyright DB InfraGO AG and contributors
-# SPDX-License-Identifier: Apache-2.0
-
-exec /opt/git/global-hooks/+pre-commit.sh $(basename "$0") "$@"
